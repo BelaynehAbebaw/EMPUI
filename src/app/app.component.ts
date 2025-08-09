bc16fc63278fd4744aff91d7a91bf4f457aec7dd
BelaynehAbebaw <belaynehabebaw38@gmail.com>
2025-08-09 14:56:38 +0300

    PUS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mgui';
+   isScreenLarge = window.innerWidth > 768;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isScreenLarge = event.target.innerWidth > 768;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatMenuModule} from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import {MatListModule} from '@angular/material/list';
+import { RouterOutlet } from '@angular/router';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-   
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule,
+    MatListModule,
+    RouterOutlet
   ],
   providers: [
     provideAnimationsAsync()
